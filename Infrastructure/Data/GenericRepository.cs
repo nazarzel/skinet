2343cc7da9fbaa848204446838b6193ab87a6f66
Mac <mac@MacBook-Pro-Mac.local>
2021-03-22 16:16:29 +0100

    added pagination, filtration, CORSes

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -21,8 +21,6 @@ namespace Infrastructure.Data
             return await _context.Set<T>().FindAsync(id);
         }
 
-
-
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _context.Set<T>().ToListAsync();
@@ -41,6 +39,11 @@ namespace Infrastructure.Data
         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
         }
     }
 }
